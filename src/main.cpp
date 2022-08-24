484a0a4d102160396d053164049e02bf4f375b33
Chloe Benz <chloe.benz@outlook.com>
2022-08-24 16:20:34 -0500

    Add function to connect to WiFi and reset credentials

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <WiFi.h>
 #include <WiFiManager.h>
 
+////////////////////////////// PROGRAM CONSTANTS ///////////////////////////////
 // Declare the pins used
 static const uint8_t loadPin = 32;
 static const uint8_t touchPin = 27;
@@ -14,7 +15,9 @@ static const int durationWatering = 1; // [s]
 // Constants
 static const int sToMilisec = 1000;
 static const int sToMicrosec = 1000000;
+////////////////////////////////////////////////////////////////////////////////
 
+/////////////////////////////// HELPER FUNCTIONS ///////////////////////////////
 void callback(){
   // Placeholder callback function
   // Should be void if touch is used as wakeup from deep sleep
@@ -28,17 +31,20 @@ void waterPlant(const int duration, const int pin){
   digitalWrite(pin, LOW);
 };
 
-void setupWiFi() {
-  // Set mode
+void resetWiFiCredentials() {
+  // ONLY USE FOR FIRST SET UP ON A NEW WiFi NETWORK
+  // Set WiFi mode
   WiFi.mode(WIFI_STA);
   Serial.begin(9600);
   // Local initialization
   WiFiManager wm;
-  // Wipe stored credentials for testing, comment for production
+  // Wipe stored credentials
   wm.resetSettings();
   // Connect to access point using password
   bool res;
   res = wm.autoConnect("AutoConnectAP", "password");
+  // From there it's manual setup (connect to AutoConnectAP WiFi network from ESP-32
+  // and enter desired WiFi network credentials)
   if (!res) {
     Serial.print("Not connected to WiFi");
   }
@@ -47,6 +53,28 @@ void setupWiFi() {
   }
 };
 
+void checkWiFi() {
+  // Set WiFi mode
+  WiFi.mode(WIFI_STA);
+  Serial.begin(9600);
+  // Local initialization
+  WiFiManager wm;
+  if (!wm.autoConnect()) {
+    Serial.print("Not connected to WiFi");
+  }
+  else {
+    Serial.print("Connected to WiFi");
+  }
+}
+
+void getTime () {
+  // Connect to WiFi network to get date and time info
+  // 
+  // Code here
+};
+////////////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////// SETUP AND LOOP ////////////////////////////////
 void setup() {
   // Initiate PIN on which load is 
   pinMode(loadPin, OUTPUT);
@@ -57,28 +85,33 @@ void setup() {
   // BY TOUCH
   touchAttachInterrupt(touchPin, callback, 30);
   esp_sleep_enable_touchpad_wakeup();
-  // BY CLOCK
 
-  // Print why we were woken up
-  Serial.begin(9600);
+  // Wake up actions by wake up source
+  Serial.begin(9600); // Open serial monitor to check wake up reason
   delay(1000); // 1 second delay to allow the Serial Monitor to open
   esp_sleep_wakeup_cause_t wakeupSource = esp_sleep_get_wakeup_cause();
   switch(wakeupSource) {
     case ESP_SLEEP_WAKEUP_TOUCHPAD: 
+      // If requested to water plant by touch, do so
       Serial.print("TOUCHPAD\n"); 
       waterPlant(durationWatering*sToMilisec, loadPin);
       break;
     case ESP_SLEEP_WAKEUP_TIMER: 
-      // waterPlant(durationWatering*sToMilisec, loadPin);
-      setupWiFi();
-      Serial.print("TIMER\n"); break;
+      // Connect to WiFi every 24hrs to check the date and water plant if is goal date
+      // Right now, timer triggers WiFi setup
+      Serial.print("TIMER\n");
+      checkWiFi();
+      break;
+    // Any other wake up source triggers nothing - so that the plant is not watered
+    // each time the ESP-32 is powered up after power loss
     default: Serial.print("DEFAULT\n"); break;
   };
 
-  // Deep sleep mode
+  // Enter deep sleep mode
   esp_deep_sleep_start();
 }
 
 void loop() {
   // Will never be called (because of deep sleep mode)
 }
+////////////////////////////////////////////////////////////////////////////////
